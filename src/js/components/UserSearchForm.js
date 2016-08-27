b643415e4ddb7cc03efb52800e92e2140b88f7d5
vschettino <A27#at14>
2016-08-27 20:06:12 -0300

    Quebra em Módulos Menores

---
+++ src/js/components/UserSearchForm.js
@@ -0,0 +1,19 @@
+import React from "react";
+export default class UserSearchForm extends React.Component {
+
+    handleInputChange(e) {
+        this.props.changeUserName(e)
+    }
+
+    handleButonClick() {
+        this.props.onClick()
+    }
+
+    render() {
+        return (<div>
+            <input onChange={this.handleInputChange.bind(this)}/>
+            <button onClick={this.handleButonClick.bind(this)}>load repos</button>
+        </div>)
+
+    }
+}
